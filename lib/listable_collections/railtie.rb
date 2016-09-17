14b8f3a812db43b6c2b94915f5af54480a3fbb5a
Mathías Montossi <mmontossi@buyin.io>
2016-09-16 22:11:41 -0300

    Fix dummy error

--- lib/listable_collections/railtie.rb
+++ lib/listable_collections/railtie.rb
@@ -2,7 +2,7 @@ module ListableCollections
   class Railtie < Rails::Railtie
 
     initializer :listable_collections do
-      ::ActiveRecord::Base.send :include ListableCollections::Extensions::ActiveRecord::Base
+      ::ActiveRecord::Base.send :include, ListableCollections::Extensions::ActiveRecord::Base
     end
 
   end
