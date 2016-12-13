d60175534a2886c85f47ede2e339de22997661c6
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 17:00:25 -0300

    Minor improvements

--- lib/listable_collections/railtie.rb
+++ lib/listable_collections/railtie.rb
@@ -1,10 +1,12 @@
 module ListableCollections
   class Railtie < Rails::Railtie
 
-    initializer 'listable_collections.extensions' do
-      ::ActiveRecord::Base.include(
-        ListableCollections::Extensions::ActiveRecord::Base
-      )
+    initializer 'listable_collections.active_record' do
+      ActiveSupport.on_load :active_record do
+        ::ActiveRecord::Base.include(
+          ListableCollections::Extensions::ActiveRecord::Base
+        )
+      end
     end
 
   end
